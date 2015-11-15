aad89280ce6008d9b63b4cb05b83fa7bca62a906
Peter Borodich <pwborodich@gmail.com>
2015-11-14 20:06:57 -0500

    user schema

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,4 +7,20 @@ module.exports = function(config) {
     db.once('open', function callback() {
         console.log('multivision db opened')
     })
+
+    var userSchema = mongoose.Schema({
+        firstName: String,
+        lastName: String,
+        userName: String
+    })
+
+    var User = mongoose.model('user', userSchema)
+
+    User.find({}).exec(function(err, collection) {
+        if(collection.length === 0) {
+            User.create({firstName: Peter, lastName: Borodich, userName: pwbwolf})
+            User.create({firstName: Emily, lastName: Runkel, userName: RunkADunk})
+            User.create({firstName: John, lastName: Papa, userName: JohnyPops})
+        }
+    })
 }
